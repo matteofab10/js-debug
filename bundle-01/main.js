99ed1e4bbe00b7aa5d83488c6401ff3f2e2cc251
matteofab10 <matteofabio10@gmail.com>
2021-12-09 17:57:00 +0100

    fix errori bundle 1

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -11,31 +11,31 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
 // 1- Stampa in console, con un ciclo for, il valore di 'i' che si incrementa ad ogni ciclo.
-// 2- No.
-// 3- Si, 'i' viene dichiarata 0 ma al ciclo diciamo di ripetersi finchè la variabile è maggiore di 5, creando un crash.
+// 2- No
+// 3- Si, dobbiamo sostituire > con <
 
 
 // ESERCIZIO 2
 function addIfEven(num) {
-    if (num % 2 = 0) {
+    if (num % 2 === 0) {
         return num + 5;
     }
     return num;
 }
 
 // 1- Aggiunge 5 ad un numero pari, se è dispari tornerà cosi com'è
-// 2- Nell'if abbiamo bisogno del doppio ==
+// 2- Nell'if l'uguale è sinonimo di dichiarazione e non uguaglianza
 // 3- No
 
 
 // ESERCIZIO 3
 function loopToFive() {
-    for (let i = 0, i < 5, i++) {
+    for (let i = 0; i < 5; i++) {
         console.log(i);
     }
 }
@@ -49,27 +49,16 @@ function loopToFive() {
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenNumbers.push(numbers[i]);
         }
-        return evenNumbers;
     }
+    console.log(evenNumbers);
+    return evenNumbers;
 }
 displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
 
 // 1- Dato un array di numeri da 1a8, controlla e restituisce i numeri pari in un altro array
-// 2- Si (modifico e commento sotto)
-// 3- Si (modifico e commento sotto)
-
-// function displayEvenNumbers(){
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i=0; i < numbers.length; i++) {
-//       if (numbers[i] % 2 === 0) {
-//         evenNumbers.push(numbers[i]);
-//       }
-//     }
-//     return evenNumbers;
-// }
-// console.log(displayEvenNumbers())
+// 2- Si, di cui vari a riga 52,53,54
+// 3- Si, dobbiamo sostituire 'i' a riga54 e dobbiamo spostare il return fuori dal ciclo
